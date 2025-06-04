f367c9a7dfd13b1acb5c2a74b1630e7afc6c5d13
McCullagh Group <martin.mccullagh@okstate.edu>
2025-06-04 14:00:35 -0500

    changed to setup.cfg

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
+from setuptools import setup
 
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name='vonMisesMixtureModel',
-    version='0.0.0',
-    author='Martin McCullagh',
-    author_email='martin.mccullagh@okstate.edu',
-    description='python/pyTorch code for determining parameters for bivariate von Mises mixture models',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url='https://github.com/mccullaghlab/vonMisesMixtureModel',
-    project_urls = {
-        "Bug Tracker": "https://github.com/mccullaghlab/vonMisesMixtureModel/issues"
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.8",
-    license='MIT',
-    install_requires=['numpy','torch>=2.0', 'scipy', 'matplotlib'],
-)
+setup()
