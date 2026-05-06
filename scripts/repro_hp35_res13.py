24fc808a81d3cf08be7a5ad2bbb98cd3b595990b
McCullagh Group <martin.mccullagh@okstate.edu>
2026-05-06 11:24:29 -0500

    added script to test numeric error issue

---
+++ scripts/repro_hp35_res13.py
@@ -0,0 +1,15 @@
+import numpy as np
+from bvvmmm import SineBVvMMM
+
+X = np.load("repro_macro3_res12_phi_psi.npy")
+
+m = SineBVvMMM(n_components=5, verbose=True, init_method="random", seed=1234)
+m.fit(X)
+print("after fit:", m.weights_, m.means_, m.kappas_)
+
+m.refine(X)
+print("after refine:", m.weights_, m.means_, m.kappas_)
+
+assert np.all(np.isfinite(m.weights_.detach().cpu().numpy()))
+assert np.all(np.isfinite(m.means_.detach().cpu().numpy()))
+assert np.all(np.isfinite(m.kappas_.detach().cpu().numpy()))
